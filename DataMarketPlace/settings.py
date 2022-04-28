48436d7577898afaf5d5a6e8c959395c7abbe6db
mobin <mobin.moghimi12@gamil.com>
2022-04-29 01:52:40 +0430

    This commit will add url to allowed host.

--- DataMarketPlace/settings.py
+++ DataMarketPlace/settings.py
@@ -118,8 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [str(BASE_DIR.joinpath('static')), ]
-# STATIC_ROOT = [str(BASE_DIR.joinpath('static')), ]
+# STATICFILES_DIRS = [str(BASE_DIR.joinpath('static')), ]
+STATIC_ROOT = [str(BASE_DIR.joinpath('static')), ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
